1dde9da1cc7786db9591eb512abdc068932db7c2
DataWorksWithSwarna <chariswarna9@gmail.com>
2026-05-09 22:05:59 +0530

    Some final changes

--- django_proj1/firstapp/models.py
+++ django_proj1/firstapp/models.py
@@ -16,6 +16,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.CharField(max_length=255)
+    reviews_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.product_name
